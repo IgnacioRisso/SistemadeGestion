7885b6728b0010a47e753770deab5bebe4048e2b
gamer <gamerdream406@gmail.com>
2022-06-29 14:23:15 -0300

    Se agrega el Proyecto

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+   Connection con; 
+   public Connection getConnection(){
+       try {
+           String myBD = "jdbc:mysql://localhost:3306/sistemaventa?serverTimeZone=UTC";
+           con = DriverManager.getConnection(myBD, "root", "");
+           return con;
+       } catch (SQLException e){
+           System.out.println(e.toString());
+       }
+           return null;
+       }
+   }
